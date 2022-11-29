444aac124590afc851038d0be28de6921b6a2fc1
FerielBOUCHAKORU <bouchakourferiel.25@gmail.com>
2022-11-29 18:52:12 +0100

    fruive

--- optimisation_projet.py
+++ optimisation_projet.py
@@ -8,28 +8,28 @@ import numpy as np
 from scipy import optimize 
 from scipy.interpolate import CubicSpline as CS
 import matplotlib.pyplot as plt
+L1=3
+L2=3
+X=np.array([4,3])
+
 def F(L1,L2,th1,th2):
     return np.array([L1*np.cos(th1)+L2*np.cos(th1+th2),L1*np.sin(th1)+L2*np.sin(th1+th2)])
 
 def F1(L1,L2,th1,th2,X):
     return np.array([L1*np.cos(th1)+L2*np.cos(th1+th2),L1*np.sin(th1)+L2*np.sin(th1+th2)])-X
 
-print(F(0.1,0.1,np.pi/2,0))
-
-
 
 def R(TH):
-
-    return list(np.array([L1*np.cos(TH[0])+L2*np.cos(TH[0]+TH[1]),L1*np.sin(TH[0])+L2*np.sin(TH[0]+TH[1])])-X)
-
+    return np.array([L1*np.cos(TH[0])+L2*np.cos(TH[0]+TH[1]),L1*np.sin(TH[0])+L2*np.sin(TH[0]+TH[1])])-X
 
 
-def Rn(TH):
-    return np.linalg.norm(np.array(R(TH)))
+
+def R_norm(TH):
+    return np.power(np.linalg.norm(np.array(R(TH))),2)
 
 def Rn2(th1,th2,L1,L2,X):
     return np.power(np.linalg.norm(F1(L1,L2,th1,th2,X)),2)
-    
+   
     
     
 def Interp(Xi,Xf,N,traj=None,Xm=None):
@@ -45,27 +45,25 @@ def Interp(Xi,Xf,N,traj=None,Xm=None):
         return [x,f(x)]
     
 
- 
-L1=0.1
-L2=0.1
-
-X=np.array([0.15,0.04])
+ # Methode 1 : Root
+print("Methode Root")
 rt=optimize.root(R, [np.pi/4,np.pi/4], jac=False)
 #print(rt.success)
 if rt.success==True:
-    print(rt.x[0],rt.x[1])
+    print(f"Les angles optimaux :{rt.x[0]},{rt.x[1]}")
     
-    print(F(L1,L2,rt.x[0],rt.x[1]))
+    print(f"Position de l'organe terminale du robot avec les angles optimaux: {F(L1,L2,rt.x[0],rt.x[1])}")
 else:
-    print(F(L1,L2,rt.x[0],rt.x[1]))
+    print(f"Position de l'organe terminale du robot : {F(L1,L2,rt.x[0],rt.x[1])}")
     print("Non reachable point")
-    
-znijo=optimize.minimize(Rn,[np.pi/4,np.pi/4])
+ # Methode 2 Minimize
+print("Methode Minimize")
+min_R_norm=optimize.minimize(R_norm,[np.pi/4,np.pi/4])
 
-print(znijo.x)
-print(F(L1,L2,znijo.x[0],znijo.x[1]))
+print(f"Les angles optimaux :{min_R_norm.x}")
+print(f"Position de l'organe terminale du robot avec les angles optimaux: {F(L1,L2,min_R_norm.x[0],min_R_norm.x[1])}")
+
 #fig=plt.figure()
-
 #plt.plot(Interp(Xi=[0.2,0],Xf=[0,0.2],N=100)[0], Interp(Xi=[0.2,0],Xf=[0,0.2],N=100)[1],'r', label = 'droite')
 
 
@@ -73,13 +71,13 @@ x,y= Interp(Xi=[0,0.2],Xf=[0.2,0],N=100,traj="Cube",Xm=[0.05,0.1])
 plt.plot(x,y,'r', label = 'droite')
 
 
-init_ang=[np.pi/4,np.pi/4]
+init_config=[np.pi/4,np.pi/4]
 """for i in range(len(x)):
     X=np.array([x[i],y[i]])
-    znijo=optimize.minimize(Rn,init_ang)
-    init_ang=znijo.x
-    #print(znijo.x[0],znijo.x[1])
-    res=F(L1,L2,znijo.x[0],znijo.x[1])
+    min_R_norm=optimize.minimize(Rn,init_config)
+    init_config=min_R_norm.x
+    #print(min_R_norm.x[0],min_R_norm.x[1])
+    res=F(L1,L2,min_R_norm.x[0],min_R_norm.x[1])
     print(res)
     #fig.plot(res[0], res[1],'k')
     plt.scatter(res[0], res[1],color="black")
@@ -113,7 +111,6 @@ while (True) :
         print("Minimum trouve")
         break
     th1n,x2n = th1n_1,th2n_1
-    print(n)
     if(n>10000) :  break 
     n+= 1 
 print( th1n_1,th2n_1)
@@ -121,3 +118,62 @@ print(f"Le nombre d'iterations pour cette methode est : {n} iterations")
 
 print(F(L1,L2, th1n_1,th2n_1))
 
+
+
+#Gradient a pas Fixe
+aplha_init = 0.3
+
+def Grad_Re(th1,th2,L1,L2,X):
+    x = X[0]
+    y = X[1]
+    dth1 = 2*x*(L1*np.sin(th1)+L2*np.sin(th1+th2))-2*y*(L1*np.cos(th1)+L2*np.cos(th1+th2))
+    dth2 = -2*L1*L2*np.sin(th2)+2*x*L2*np.sin(th1+th2)-2*y*L2*np.sin(th1+th2)
+    return np.array([dth1,dth2])
+def GradienDecent (TH0,alpha,L1,L2,eps,n_max):
+    Un = 1
+    divide = False
+    thn = TH0 
+    n=0
+    while(np.linalg.norm(Un)>eps and n<n_max ) :
+        thn_1 =  thn - alpha * Grad_Re(thn[0],thn[1],L1,L2,X)
+        if (R_norm(thn)<R_norm(thn_1)) and not divide  : 
+            alpha = aplha_init / 2 
+            divide = True
+        elif(R_norm(thn)>R_norm(thn_1) and divide):
+            alpha = aplha_init
+            divide = False
+        Un = thn_1 - thn 
+        thn = thn_1
+        n=n+1
+        print(alpha)
+    return thn
+TH00 = np.array([0,0])
+resultat = GradienDecent(TH00,alpha,L1,L2,10e-4,100) 
+print("methode du gradient")
+print(f"les angles optimaux : { resultat}")
+print(f"Position de l'organe terminale du robot avec les angles optimaux: {F(L1,L2,resultat[0],resultat[1])}")
+"""
+# Définition du domaine de tracé
+xmin, xmax, nx = -np.pi, np.pi, 100
+ymin, ymax, ny = -np.pi, np.pi, 100
+# Discrétisation du domaine de tracé
+x1d = np.linspace(xmin,xmax,nx)
+y1d = np.linspace(ymin,ymax,ny)
+x2d, y2d = np.meshgrid(x1d, y1d)
+print(np.shape(x2d),np.shape(y2d))
+# Tracé des isovaleurs de f1
+nIso = 50
+#Tracé des isovaleur pour une seule iteration 
+plt.figure("fig")
+res = []
+
+for i in x1d:
+    for j in y1d:
+        res.append(R_norm(np.array([i,j])))
+print(res)        
+
+plt.contour(x2d,y2d,res,nIso)
+plt.title('Isovaleurs')
+plt.xlabel('Valeurs de x1')
+plt.ylabel('Valeurs de x2')
+plt.grid()"""
