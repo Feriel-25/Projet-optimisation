ec810bffb3919e39f224ba9accd06d153f43f357
Guedouah Khaled <khaledguedouah2000@gmail.com>
2022-11-30 22:42:30 +0100

    changes

--- optimisation_projet.py
+++ optimisation_projet.py
@@ -10,7 +10,7 @@ from scipy.interpolate import CubicSpline as CS
 import matplotlib.pyplot as plt
 L1=3
 L2=3
-X=np.array([4,3])
+X=np.array([2,1])
 
 def F(L1,L2,th1,th2):
     return np.array([L1*np.cos(th1)+L2*np.cos(th1+th2),L1*np.sin(th1)+L2*np.sin(th1+th2)])
@@ -20,7 +20,7 @@ def F1(L1,L2,th1,th2,X):
 
 
 def R(TH):
-    return np.array([L1*np.cos(TH[0])+L2*np.cos(TH[0]+TH[1])-X[0],L1*np.sin(TH[0])+L2*np.sin(TH[0]+TH[1])]-X[1])
+    return np.array([L1*np.cos(TH[0])+L2*np.cos(TH[0]+TH[1])-X[0],L1*np.sin(TH[0])+L2*np.sin(TH[0]+TH[1])-X[1]])
 
 
 
@@ -46,29 +46,28 @@ def Interp(Xi,Xf,N,traj=None,Xm=None):
     
 
  # Methode 1 : Root
-print("Methode Root")
+print("Methode Root : ")
 rt=optimize.root(R, [np.pi/4,np.pi/4], jac=False)
 #print(rt.success)
 if rt.success==True:
-    print(f"Les angles optimaux :{rt.x[0]},{rt.x[1]}")
+    print(f"Les angles optimaux = {rt.x} \nLe point (X,Y) ={F(L1,L2,rt.x[0],rt.x[1])}")
     
-    print(f"Position de l'organe terminale du robot avec les angles optimaux: {F(L1,L2,rt.x[0],rt.x[1])}")
 else:
-    print(f"Position de l'organe terminale du robot : {F(L1,L2,rt.x[0],rt.x[1])}")
     print("Non reachable point")
  # Methode 2 Minimize
-print("Methode Minimize")
-min_R_norm=optimize.minimize(R_norm,[np.pi/4,np.pi/4])
 
-print(f"Les angles optimaux :{min_R_norm.x}")
-print(f"Position de l'organe terminale du robot avec les angles optimaux: {F(L1,L2,min_R_norm.x[0],min_R_norm.x[1])}")
+min_R_norm=optimize.minimize(R_norm,[np.pi/4,np.pi/4])
+#print(min_R_norm.success)
+print("Methode Minimize : ")
+print(f"les angles optimaux ={min_R_norm.x} \nLe point (X,Y) ={F(L1,L2,min_R_norm.x[0],min_R_norm.x[1])}")
+
 
 #fig=plt.figure()
 #plt.plot(Interp(Xi=[0.2,0],Xf=[0,0.2],N=100)[0], Interp(Xi=[0.2,0],Xf=[0,0.2],N=100)[1],'r', label = 'droite')
 
 
-x,y= Interp(Xi=[0,0.2],Xf=[0.2,0],N=100,traj="Cube",Xm=[0.05,0.1])
-plt.plot(x,y,'r', label = 'droite')
+#x,y= Interp(Xi=[0,0.2],Xf=[0.2,0],N=100,traj="Cube",Xm=[0.05,0.1])
+#plt.plot(x,y,'r', label = 'droite')
 
 
 init_config=[np.pi/4,np.pi/4]
@@ -87,14 +86,15 @@ init_config=[np.pi/4,np.pi/4]
 
 # Gradient Pas optimal
 
-
+"""
 def Grad_Re(th1,th2,L1,L2,X):
     x = X[0]
     y = X[1]
     dth1 = 2*x*(L1*np.sin(th1)+L2*np.sin(th1+th2))-2*y*(L1*np.cos(th1)+L2*np.cos(th1+th2))
     dth2 = -2*L1*L2*np.sin(th2)+2*x*L2*np.sin(th1+th2)-2*y*L2*np.sin(th1+th2)
     return np.array([dth1,dth2])
-
+"""
+"""
 th1n = 0
 th2n = 0
 n = 1
@@ -117,14 +117,14 @@ print( th1n_1,th2n_1)
 print(f"Le nombre d'iterations pour cette methode est : {n} iterations")    
 
 print(F(L1,L2, th1n_1,th2n_1))
-
+"""
 #plotting
 
 def animate (TH): 
-    plt.xlim([-2, 10])
-    plt.ylim([-2,10])
-    L1=3
-    L2=3
+    plt.xlim([-2, 2])
+    plt.ylim([-2,2])
+    L1=0.5
+    L2=0.5
     x1, y1 = [0, L1*np.cos(TH[0])], [0, L1*np.sin(TH[0])]
     x2, y2 = [L1*np.cos(TH[0]),L1*np.cos(TH[0])+L2*np.cos(TH[0]+TH[1])], [L1*np.sin(TH[0]),L1*np.sin(TH[0])+ L2*np.sin(TH[0]+TH[1])]
  
@@ -136,38 +136,36 @@ def animate (TH):
 
 
 #Gradient a pas Fixe
-aplha_init = 0.3
+aplha_init = 0.05
 
 def Grad_Re(th1,th2,L1,L2,X):
     x = X[0]
     y = X[1]
     dth1 = 2*x*(L1*np.sin(th1)+L2*np.sin(th1+th2))-2*y*(L1*np.cos(th1)+L2*np.cos(th1+th2))
-    dth2 = -2*L1*L2*np.sin(th2)+2*x*L2*np.sin(th1+th2)-2*y*L2*np.sin(th1+th2)
+    dth2 = -2*L1*L2*np.sin(th2)+2*x*L2*np.sin(th1+th2)-2*y*L2*np.cos(th1+th2)
     return np.array([dth1,dth2])
 def GradienDecent (TH0,alpha,L1,L2,eps,n_max,x):
     Un = 1
-    divide = False
+    #divide = False
     thn = TH0 
     n=0
     while(np.linalg.norm(Un)>eps and n<n_max ) :
         thn_1 =  thn - alpha * Grad_Re(thn[0],thn[1],L1,L2,x)
-        if (R_norm(thn)<R_norm(thn_1)) and not divide  : 
-            alpha = aplha_init / 2 
-            divide = True
-        elif(R_norm(thn)>R_norm(thn_1) and divide):
-            alpha = aplha_init
-            divide = False
+       # if (R_norm(thn)<R_norm(thn_1)) and not divide  : 
+        #    alpha = aplha_init / 2 
+         #   divide = True
+        #elif(R_norm(thn)>R_norm(thn_1) and divide):
+         #   alpha = aplha_init
+          #  divide = False
         Un = thn_1 - thn 
         thn = thn_1
         n=n+1
     return thn
-TH00 = np.array([0,0])
-plt.figure(figsize=(6,6))
-resultat = GradienDecent(TH00,alpha,L1,L2,10e-4,100,X) 
 
-print("methode du gradient")
-print(f"les angles optimaux : { resultat}")
-print(f"Position de l'organe terminale du robot avec les angles optimaux: {F(L1,L2,resultat[0],resultat[1])}")
+resultat = GradienDecent(init_config,aplha_init,L1,L2,10e-4,100,X) 
+print("Methode du gradient a pas fixe : ")
+print(f"les angles optimaux = {resultat} \nLe point (X,Y) = {F(L1,L2,resultat[0],resultat[1])}")
+
 
 # Définition du domaine de tracé
 xmin, xmax, nx = -np.pi, np.pi, 100
@@ -176,24 +174,24 @@ ymin, ymax, ny = -np.pi, np.pi, 100
 x1d = np.linspace(xmin,xmax,nx)
 y1d = np.linspace(ymin,ymax,ny)
 x2d, y2d = np.meshgrid(x1d, y1d)
-print(np.shape(x2d),np.shape(y2d))
 # Tracé des isovaleurs de f1
 nIso = 100
 #Tracé des isovaleur pour une seule iteration 
 plt.figure("fig")
 
 plt.contour(x2d,y2d,R_norm([x2d,y2d]),nIso)
+plt.scatter(resultat[0],resultat[1], s=100,marker='x',color='r')
 plt.title('Isovaleurs')
 plt.xlabel('Valeurs de x1')
 plt.ylabel('Valeurs de x2')
 plt.grid()
-
-Xf=[2,2]
-Xi=[0,6]
+"""
+Xf=[0,0.5]
+Xi=[0.5,0.5]
 N=50
 x,y=Interp(Xi,Xf,N)
-
-
+"""
+"""
 fig = plt.figure()
 
 for i in  range(N): 
@@ -204,3 +202,4 @@ for i in  range(N):
     animate(rt.x)
     plt.plot(x,y,'r', label = 'droite')
     #plt.scatter(x[i],y[i], s=100,marker='x',color='r',linewidths=20)
+    """
