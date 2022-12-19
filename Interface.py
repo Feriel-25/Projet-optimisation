fd928b0ddd8cd78015362e6bb7cfec7bea54c556
Guedouah Khaled <khaledguedouah2000@gmail.com>
2022-12-19 22:01:28 +0100

    final

--- Interface.py
+++ Interface.py
@@ -8,7 +8,7 @@ from scipy import optimize
 from Functions import R , R_norm , F
 from Gradient import  *
 from Newton import Newton
-from Graphics import  isoValeurs
+from Graphics import  isoValeurs, animate
 import matplotlib.pyplot as plt
 
 def Interface(L1,L2):
@@ -29,12 +29,26 @@ def Interface(L1,L2):
     while (True) :
         m = input("1 : Scipy.Optimize  \n2 : Scipy.Minimize \n3 : Gradient \n4 : Newton\n ")
         if (m=='1') :
-            resultat = optimize.root(R, [np.pi/4,np.pi/4],args = Params, jac=False)
+            print("Veuillez saisir une suposition initialle de la solution du probleme : ")
+            theta1 =  float(input("theta 1 : ..."))
+            theta2 =  float(input("theta 2 : ..."))
+            supInit = [theta1,theta2]
+            resultat = optimize.root(R, supInit,args = Params, jac=False)
             print(f"les angles optimaux = {resultat.x} \nLe point X = {F(L1,L2,resultat.x[0],resultat.x[1])}")
+            plt.figure()
+            animate(resultat.x,Params)
+            plt.grid()
             break
         elif (m=='2') :
-            resultat = optimize.minimize(R_norm,[np.pi/4,np.pi/4],args = Params)
+            print("Veuillez saisir une suposition initialle de la solution du probleme : ")
+            theta1 =  float(input("theta 1 : ..."))
+            theta2 =  float(input("theta 2 : ..."))
+            supInit = [theta1,theta2]
+            resultat = optimize.minimize(R_norm,supInit,args = Params)
             print(f"les angles optimaux = {resultat.x} \nLe point X = {F(L1,L2,resultat.x[0],resultat.x[1])}")
+            plt.figure()
+            animate(resultat.x,Params)
+            plt.grid()
             break 
 
         elif (m == '3'):
@@ -43,11 +57,17 @@ def Interface(L1,L2):
             eps = float(input("Veuillez entrer la valeur de la precision epsilon : ..."))
             resultat , converge = GradienDecentAmeliore(ConfigInit,aplhaInit,Params,eps,n_max,IsoV=IsoV ,Disp=False)
             print(f"les angles optimaux = {resultat} \nLe point X = {F(L1,L2,resultat[0],resultat[1])}")
+            plt.figure()
+            animate(resultat,Params)
+            plt.grid()
             break 
         elif (m =='4'):
             eps = float(input("Veuillez entrer la valeur de la precision epsilon : ..."))
             nmax = int(input("Veuillez entrer la valeur maximale d'iteration : ..."))
             resultat,success=Newton(ConfigInit,Params,eps,nmax,Disp=False,IsoV=IsoV)
+            plt.figure()
+            animate(resultat,Params)
+            plt.grid()
             break 
 
         else :
